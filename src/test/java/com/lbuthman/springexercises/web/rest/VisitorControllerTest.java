6c7bef72201037f21b0611fa3316599f7b7f6bf2
Luke Buthman <lbuthman@gmail.com>
2017-12-23 19:22:33 -0600

    [refactor] Refactor test for better coverage

--- src/test/java/com/lbuthman/springexercises/web/rest/VisitorControllerTest.java
+++ src/test/java/com/lbuthman/springexercises/web/rest/VisitorControllerTest.java
@@ -1,42 +1,93 @@
 package com.lbuthman.springexercises.web.rest;
 
+import com.lbuthman.springexercises.SpringExercisesApplication;
+import com.lbuthman.springexercises.domain.Visitor;
+import com.lbuthman.springexercises.repositories.VisitorRepository;
 import com.lbuthman.springexercises.services.VisitorService;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
+import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
-public class VisitorResourceTest {
+import static org.hamcrest.Matchers.hasItems;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-    @Mock
-    private VisitorResource visitorResource;
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringExercisesApplication.class)
+public class VisitorControllerTest {
 
-    @Mock
-    private VisitorService visitorService;
+    private final static String DEFAULT_NAME = "Luke";
+    private final static String UPDATED_NAME = "Calvin";
+
+    @MockBean
+    private VisitorService service;
+
+    @Autowired
+    private VisitorRepository repository;
 
     private MockMvc mockMvc;
 
+    private Visitor visitor;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(visitorResource).build();
+        final VisitorController controller = new VisitorController(service);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
-    @Test
-    public void getAllVisitors() {
+    /**
+     * Create a Visitor entity for tests
+     */
+    public static Visitor createVisitor() {
+        Visitor visitor = new Visitor();
+        visitor.setName(DEFAULT_NAME);
+        return visitor;
     }
 
-    @Test
-    public void createVisitor() {
+    /**
+     * Initialize default visitor before each test runs
+     */
+    @Before
+    public void initTest() {
+        visitor = createVisitor();
     }
 
+    @Transactional
     @Test
-    public void updateVisitor() {
+    public void getAllVisitors() throws Exception {
+        //initialize repository
+        repository.saveAndFlush(visitor);
+
+        when(service.getAllVisitors()).thenReturn(repository.findAll());
+
+        //when
+        mockMvc.perform(get("/api/v1/visitors"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.[*].id").value(hasItems(visitor.getId().intValue())))
+                .andExpect(jsonPath("$.[*].name").value(hasItems(DEFAULT_NAME)));
     }
 
-    @Test
-    public void deleteVisitor() {
-    }
+//    @Test
+//    public void createVisitor() {
+//    }
+//
+//    @Test
+//    public void updateVisitor() {
+//    }
+//
+//    @Test
+//    public void deleteVisitor() {
+//    }
 }
