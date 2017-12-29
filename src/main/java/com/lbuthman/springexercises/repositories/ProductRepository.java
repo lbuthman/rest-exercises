f11606a38bfa0e6a3663601c16af61e288a3fef6
Luke Buthman <lbuthman@gmail.com>
2017-12-29 13:02:42 -0600

    [feat] Add repository for products

---
+++ src/main/java/com/lbuthman/springexercises/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.lbuthman.springexercises.repositories;
+
+import com.lbuthman.springexercises.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
