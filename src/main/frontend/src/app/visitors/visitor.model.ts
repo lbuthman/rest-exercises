6eb5be8bc95574a10135dd6d9da7f9b358f741d2
Luke Buthman <lbuthman@gmail.com>
2018-01-01 19:45:06 -0600

    [feat] Upgrade visitor list to use object

---
+++ src/main/frontend/src/app/visitors/visitor.model.ts
@@ -0,0 +1,10 @@
+export class Visitor {
+
+  public name: string;
+  public greeted: boolean;
+
+  constructor(name: string, greeted: boolean) {
+    this.name = name;
+    this.greeted = greeted;
+  }
+}
--- src/main/frontend/src/app/visitors/visitors-list/visitors-list.component.ts
+++ src/main/frontend/src/app/visitors/visitors-list/visitors-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Visitor} from "../visitor.model";
 
 @Component({
   selector: 'visitors-list',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisitorsListComponent implements OnInit {
 
+  visitors: Visitor[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.visitors.push(new Visitor("Luke", true));
+    this.visitors.push(new Visitor("Albie", false));
+    this.visitors.push(new Visitor("Julie", false));
+  }
+
+  onVisitorChange($event, visitor) {
+    console.log("checkbox checked")
   }
 
 }
